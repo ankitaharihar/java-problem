4118716c1965a7960cb8085671b52dc2d316c436
Ankita <ankitaharihar81@gmail.com>
2026-03-08 17:01:00 +0530

    Add initial implementation of Prime Number and Palindrome checks

---
+++ Main.java
@@ -0,0 +1,44 @@
+// Check Prime Number
+//public class Main{
+//     public static void main(String[] args) {
+
+//         int n = 7;
+//         boolean isPrime = true;
+
+//         for(int i=2;i<=Math.sqrt(n);i++){
+//             if(n % i == 0){
+//                 isPrime = false;
+//                 break;
+//             }
+//         }
+
+//         if(isPrime)
+//             System.out.println("Prime");
+//         else
+//             System.out.println("Not Prime");
+//     }
+// }
+
+
+//Check Palindrome
+// public class Main {
+//     public static void main (String[]args){
+//         int a =1446;
+//         int org = a;
+//         int reve = 0 ;
+//     while(a>0){
+//     int digit = a% 10;
+//     reve = reve *10 + digit;
+//     a = a/10;
+//     }
+
+    
+//     if(org==reve){
+//         System.out.println("Palin");
+//     }else{
+//         System.out.println("not");
+//     }
+// }
+// }
+
+
