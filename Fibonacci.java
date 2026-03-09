2d9db573606195d936949fa3250934bc5d0c3f65
Ankita <ankitaharihar81@gmail.com>
2026-03-09 09:35:55 +0530

    Add Fibonacci series implementation and restore Prime Number and Palindrome checks

--- Fibonacci.java
+++ Fibonacci.java
@@ -42,3 +42,24 @@
 // }
 
 
+// fibonacci series
+// public class Fibonacci {
+
+//     public static void main(String[] args) {
+
+//         int n = 10;
+//         int a = 0;
+//         int b = 1;
+
+//         System.out.print(a + " " + b + " ");
+
+//         for(int i = 2; i < n; i++){
+//             int c = a + b;
+//             System.out.print(c + " ");
+//             a = b;
+//             b = c;
+//         }
+
+//     }
+
+// }
